a9cc2fb7ee60c2633927a571782547be34c9b66f
Ragupathy Rathinasabapathy <e0267619@u.nus.edu>
2018-11-27 17:49:28 +0800

    updates to deploy to heroku 3

--- src/server.js
+++ src/server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/my-Huifu'));
+app.use(express.static(__dirname + '/dist/my-Huifu'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/my-Huifu/index.html'));
+res.sendFile(path.join(__dirname+'/dist/my-Huifu/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
