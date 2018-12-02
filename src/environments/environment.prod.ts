e3775fa303698f8e3db395e7df5cf62de028a299
Ragupathy Rathinasabapathy <e0267619@u.nus.edu>
2018-12-02 19:52:18 +0800

    commit after login v2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCokLQXaaiOMup9ASMcOb9ikdpyuvMbvJ4",
+    authDomain: "my-huifu-1543306021160.firebaseapp.com",
+    databaseURL: "https://my-huifu-1543306021160.firebaseio.com",
+    projectId: "my-huifu-1543306021160",
+    storageBucket: "my-huifu-1543306021160.appspot.com",
+    messagingSenderId: "1029883958097"
+  }
 };
